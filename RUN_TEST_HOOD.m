38a01c268a0130930ea8cc75f9f77d69ab514507
Rory Hadden <56675143+rorymiles@users.noreply.github.com>
2021-04-03 08:10:21 +0100

    Add files via upload
    Added the ability to log data from an external data logger

--- RUN_TEST_HOOD.m
+++ RUN_TEST_HOOD.m
@@ -1,6 +1,7 @@
 clear all
 close all
 clc
+instrreset
 
 %This programme runs a test in the Hood
 
@@ -9,7 +10,7 @@ clc
 %%%% NOTES %%%
 % - the DPT span is from manufacurers data sheet - this should be
 %checked.
-% - the time between readings is ~1.3 s. 
+% - the time between readings is ~1.3 s.
 % - the HRR calculations may be spurious.
 % - delay times are not considered.
 % - the RH probe is assumed to follow manufacturer data (is the hard coded data correct?).
@@ -22,26 +23,102 @@ clc
 % - you need to write a separate programme to do proper HRR calculations.
 % - all data are saved as a .mat file
 
-% Tell matlab the format for the timestamp - required for keepign track of
+
+% Tell matlab the format for the timestamp - required for keeping track of
 % things
 TimeStampFormat = 'yyyymmddHHMMSS.FFF';
 
 %give a filename for your data. The timestamp should prevent any overwriting
-FileName = inputdlg('Filename', 'Filename', [1 35], {[datestr(now, TimeStampFormat) '_']});
-FileName = [cell2mat(FileName) '.mat'];
+FileName = inputdlg('Filename', 'Filename', [1 35], {[datestr(now, TimeStampFormat) '_Hood Data']});
+FileName = [cell2mat(FileName)];
 
-% AGILENT
+% AGILENT -hood
 % Conenct
 v34970A = visa('agilent', 'GPIB0::9::INSTR');
 v34970A.InputBufferSize = 8388608;
 v34970A.ByteOrder = 'littleEndian';
 fopen(v34970A);
-
+fprintf(v34970A,'*RST');
 % Housekeeping
+set(v34970A,'timeout',2)
+HOOD_CHANNELS_TC = '111:115';
+HOOD_CHANNELS_VDC = '101:109, 116';
+
 fprintf(v34970A, sprintf(':FORMat:READing:CHANnel %d', 1));
 fprintf(v34970A, sprintf(':FORMat:READing:ALARm %d', 1));
 fprintf(v34970A, sprintf(':FORMat:READing:UNIT %d', 1));
 fprintf(v34970A, sprintf(':FORMat:READing:TIME:TYPE %s', 'REL'));
+
+fprintf(v34970A,sprintf(['CONF:VOLT:DC, (@' HOOD_CHANNELS_VDC ')']));             % Configure the specified channels for dc or ac voltage measurements but do not initiate the scan. Note that this command also redefines the scan list. The rage is 10V and the resolution 1PLC (3E-5 or 5 1/2 Digits or 20 Bits)
+fprintf(v34970A,sprintf(['SENS:RES:APER, 1,(@' HOOD_CHANNELS_VDC ')']));
+
+% fprintf(v34970A,sprintf(['CONF:VOLT:DC 10  ,0.001   ,(@' HOOD_CHANNELS_VDC ')']));             % Configure the specified channels for dc or ac voltage measurements but do not initiate the scan. Note that this command also redefines the scan list. The rage is 10V and the resolution 1PLC (3E-5 or 5 1/2 Digits or 20 Bits)
+
+fprintf(v34970A,sprintf(['CONF:TEMP TC, K,(@' HOOD_CHANNELS_TC ')']));                      % Configure the specified channels for thermocouple measurements but do not initiate the scan. Note that this command also redefines the scan list. The default (DEF) transducer type is a J-Type thermocouple.
+fprintf(v34970A,sprintf(['UNIT:TEMP K,(@' HOOD_CHANNELS_TC ')']));                          % Select the temperature measurement units on the specified channels. The default is “C”. The :TEMP? query returns the temperature measurement units currently selected. Returns “C”, “F”, or “K”.
+fprintf(v34970A,sprintf(['SENS:TEMP:TRAN:TYPE TC,(@' HOOD_CHANNELS_TC ')']));               % Select the type of temperature transducer to use for measurements on the specified channels. Select from TC (thermocouple), RTD (2-wire RTD), FRTD (4-wire RTD), or THER (thermistor) . The default is TC. The :TYPE? query returns the current temperature transducer type on the specified channels. Returns “TC”, “RTD”, “FRTD”, or “THER”.
+fprintf(v34970A,sprintf(['SENS:TEMP:TRAN:TC:TYPE K,(@' HOOD_CHANNELS_TC ')']));             % Select the thermocouple type to use on the specified channels. The default is a J-Type thermocouple. The :TYPE? query returns the thermocouple type currently in use. Returns “B”, “E”, “J”, “K”, “N”, “R”, “S”, or “T”.
+fprintf(v34970A,sprintf(['SENS:TEMP:TRAN:TC:CHEC OFF,(@' HOOD_CHANNELS_TC ')']));           % Disable or enable the thermocouple check feature to verify that your thermocouples are properly connected to the screw terminals for measurements. If you enable this feature, the instrument measures the channel resistance after each thermocouple measurement to ensure a proper connection. If an open connection is detected (greater than 5 k? on the 10 k? range), the instrument reports an overload condition for that channel. The default is “OFF”. The :CHEC? query returns the thermocouple check setting. Returns “0” (OFF) or “1” (ON).
+fprintf(v34970A,sprintf(['SENS:TEMP:TRAN:TC:RJUN:TYPE INT,(@' HOOD_CHANNELS_TC ')']));
+
+fprintf(v34970A,sprintf(['ROUT:SCAN (@' HOOD_CHANNELS_TC ',' HOOD_CHANNELS_VDC ')']));
+
+% a prompt in the command line to check the TC data is beign recorded
+TCs_in_use = input('Are you using temperature measurements? y/n: ', 's');
+
+if sum(strcmp(TCs_in_use, {'Y', 'y'})) >0
+    
+    % AGILENT - temps
+    v34980A = visa('agilent', 'USB0::0x0957::0x0507::MY53157045::0::INSTR');
+    v34980A.InputBufferSize = 8388608;
+    v34980A.ByteOrder = 'littleEndian';
+    fopen(v34980A);
+    fprintf(v34980A,'*RST')
+    
+    fprintf(v34980A,'*CLS')
+    
+    % Housekeeping
+    set(v34980A,'timeout',2)
+    ScanList_Temp = '1001:1006, 1011:1016';
+    ScanList_RTD = ['1020'];
+    ScanList_HFG = ['3002:3004'];
+    %
+    fprintf(v34980A, sprintf(':FORMat:READing:CHANnel %d', 1));
+    fprintf(v34980A, sprintf(':FORMat:READing:ALARm %d', 1));
+    fprintf(v34980A, sprintf(':FORMat:READing:UNIT %d', 1));
+    fprintf(v34980A, sprintf(':FORMat:READing:TIME:TYPE %s', 'REL'));
+    
+    %     configure the datalogger to give the right measurement type
+    fprintf(v34980A, sprintf(['CONF:TEMP TC, K, (@' ScanList_Temp ')']));
+    fprintf(v34980A, sprintf(['UNIT:TEMP, K, (@' ScanList_Temp ')']));
+    fprintf(v34980A, sprintf(['SENS:TEMP:TRAN:TYPE TC, (@' ScanList_Temp ')']));
+    fprintf(v34980A, sprintf(['SENS:TEMP:TRAN:TC:TYPE K, (@' ScanList_Temp ')']));
+    
+    %
+    fprintf(v34980A, sprintf(['UNIT:TEMP, C, (@' ScanList_RTD ')']));
+    fprintf(v34980A, sprintf(['CONF:TEMP FRTD,91,(@' ScanList_RTD ')']));
+    fprintf(v34980A, sprintf(['TEMP:TRAN:FRTD:REF ON,(@' ScanList_RTD ')']))
+    fprintf(v34980A, sprintf(['SENS:TEMP:TRAN:TC:RJUN:TYPE EXT, (@' ScanList_Temp ')']))
+    
+    
+    %configure any voltage channels
+    fprintf(v34970A,sprintf(['CONF:VOLT:DC, (@' ScanList_HFG ')']));             % Configure the specified channels for dc or ac voltage measurements but do not initiate the scan. Note that this command also redefines the scan list. The rage is 10V and the resolution 1PLC (3E-5 or 5 1/2 Digits or 20 Bits)
+    %fprintf(v34970A,sprintf(['SENS:RES:APER, 1,(@' ScanList_HFG ')']));
+    %
+    
+    %         fprintf(v34980A,sprintf(['CONF:TEMP TC, K,(@' ScanList_Temp ')']));                      % Configure the specified channels for thermocouple measurements but do not initiate the scan. Note that this command also redefines the scan list. The default (DEF) transducer type is a J-Type thermocouple.
+    %         fprintf(v34980A,sprintf(['UNIT:TEMP C,(@' ScanList_Temp ')']));                          % Select the temperature measurement units on the specified channels. The default is “C”. The :TEMP? query returns the temperature measurement units currently selected. Returns “C”, “F”, or “K”.
+    %         fprintf(v34980A,sprintf(['SENS:TEMP:TRAN:TYPE TC,(@' ScanList_Temp ')']));               % Select the type of temperature transducer to use for measurements on the specified channels. Select from TC (thermocouple), RTD (2-wire RTD), FRTD (4-wire RTD), or THER (thermistor) . The default is TC. The :TYPE? query returns the current temperature transducer type on the specified channels. Returns “TC”, “RTD”, “FRTD”, or “THER”.
+    %         fprintf(v34980A,sprintf(['SENS:TEMP:TRAN:TC:TYPE K,(@' ScanList_Temp ')']));             % Select the thermocouple type to use on the specified channels. The default is a J-Type thermocouple. The :TYPE? query returns the thermocouple type currently in use. Returns “B”, “E”, “J”, “K”, “N”, “R”, “S”, or “T”.
+    %         fprintf(v34980A,sprintf(['SENS:TEMP:TRAN:TC:CHEC OFF,(@' ScanList_Temp ')']));           % Disable or enable the thermocouple check feature to verify that your thermocouples are properly connected to the screw terminals for measurements. If you enable this feature, the instrument measures the channel resistance after each thermocouple measurement to ensure a proper connection. If an open connection is detected (greater than 5 k? on the 10 k? range), the instrument reports an overload condition for that channel. The default is “OFF”. The :CHEC? query returns the thermocouple check setting. Returns “0” (OFF) or “1” (ON).
+    %         fprintf(v34980A,sprintf(['SENS:TEMP:TRAN:TC:RJUN:TYPE INT,(@' ScanList_Temp ')']));
+    %         fprintf(v34970A,sprintf(['ROUT:SCAN (@' ScanList_Temp ')']));
+    
+    fprintf(v34980A, ['ROUTE:SCAN (@' [ScanList_RTD ',' ScanList_Temp ',' ScanList_HFG] ')'])
+    
+else
+    v34980A = 1;
+end
 
 % a prompt in the command line to check the load cell is in use
 loadcell_in_use = input('Are you using the load cell? y/n: ', 's');
@@ -78,7 +155,7 @@ stop_button = uicontrol('Style', 'Pushbutton', 'String', 'Stop', 'fontsize', 16,
 i = 1; %cos a counter is needed to index the variables
 
 %this is a hack
-v_duct = 0; 
+v_duct = 0;
 X0_O2_A = 0;
 X0_CO_A = 0;
 X0_CO2_A = 0;
@@ -99,20 +176,34 @@ while(ishandle(stop_button)) %so the loop runs til you press STOP
         mass(i) = NaN;
     end
     
-    temps = query(v34970A, sprintf(':MEASure:TEMPerature? %s,%s,(%s)', 'TCouple', 'K', '@111:115')); %scan the temperaure channels
     tic
-    readings = query(v34970A, sprintf(':MEASure:VOLTage:DC? (%s)', '@101:109')); %scan the voltage reacdings (DPT, gases)
+    %         TC_data = query(v34980A, sprintf(':MEASure:TEMPerature? %s,%s,(%s)', 'TCouple', 'K', '@1001:1008,1011:1018')); %scan the temperaure channels)
+    %         temps = query(v34970A, sprintf(':MEASure:TEMPerature? %s,%s,(%s)', 'TCouple', 'K', '@111:115')); %scan the temperaure channels
+    %         readings = query(v34970A, sprintf(':MEASure:VOLTage:DC? (%s)', '@101:109')); %scan the voltage reacdings (DPT, gases)
+    hood_data_as_text = query(v34970A, 'READ?');
+    data_as_num = str2num(hood_data_as_text);
+    readings = data_as_num([1:9 15]);
+    temps = data_as_num(10:14);
+    
+    if isobject(v34980A)==1
+        TC_data_as_text = query(v34980A, 'READ?');
+        TC_data = str2num(TC_data_as_text);
+        TC_Temps(i,:) = TC_data(1:13);
+        HFG_VDC(i,:) = TC_data(14:16);
+        TC_data(1:13)
+        TC_data(14:16)
+    end
     toc
     
-    v_data(i,:) = str2num(readings); %make a variable that is a matriix of all teh channels
-    temperatures(i,:) = str2num(temps) ; %same with the temps
+    v_data(i,:) = (readings); %make a variable that is a matrix of all teh channels
+    temperatures(i,:) = (temps) ; %same with the temps
     timestamp(i) = str2double(datestr(now, TimeStampFormat))'; %get a timestamp to associate with the data
     test_time(i) = (datenum(num2str(timestamp(i), '%.3f'), TimeStampFormat)-datenum(num2str(timestamp(1), '%.3f'), TimeStampFormat)).*(24*60*60);
     
-    O2_conc(i) = v_data(i,1)*O2_fit(1)+O2_fit(2);
+    conc_O2(i) = v_data(i,1)*O2_fit(1)+O2_fit(2);
     DPT_Pa(i) = v_data(i,2)*DPT_fit(1)+DPT_fit(2);
-    CO_conc(i) = v_data(i,3)*CO_fit(1)+CO_fit(2);
-    CO2_conc(i) = v_data(i,4)*CO2_fit(1)+CO2_fit(2);
+    conc_CO(i) = v_data(i,3)*CO_fit(1)+CO_fit(2);
+    conc_CO2(i) = v_data(i,4)*CO2_fit(1)+CO2_fit(2);
     
     RH_ambient(i) = v_data(i,9)*RH_fit(1)+RH_fit(2);
     
@@ -122,24 +213,36 @@ while(ishandle(stop_button)) %so the loop runs til you press STOP
     T_duct2(i) =  temperatures(i,4);
     T_ambient(i) = temperatures(i,5);
     
-    [Q_OC__O2_CO2_CO, v_duct, X0_O2_A, X0_CO_A, X0_CO2_A] = HRR_calc(i, CO_conc, CO2_conc, O2_conc, T_ambient, T_duct, RH_ambient, DPT_Pa, v_duct, X0_O2_A, X0_CO_A, X0_CO2_A, Q_OC__O2_CO2_CO);
+    [Q_OC__O2_CO2_CO, v_duct, X0_O2_A, X0_CO_A, X0_CO2_A] = HRR_calc(i, conc_CO, conc_CO2, conc_O2, T_ambient, T_duct, RH_ambient, DPT_Pa, v_duct, X0_O2_A, X0_CO_A, X0_CO2_A, Q_OC__O2_CO2_CO);
     
     tic
-    plot_O2 = plot(test_time, O2_conc, 'parent', O2_axes, 'color', 'b');
+    plot_O2 = plot(test_time, conc_O2, 'parent', O2_axes, 'color', 'b');
     plot_mass = plot(test_time, mass, 'parent', mass_axes, 'color', 'k');
-    plot_CO = plot(test_time, CO_conc, 'parent', CO_axes, 'color', 'k');
-    plot_CO2 = plot(test_time, CO2_conc, 'parent', CO2_axes, 'color', 'r');
+    plot_CO = plot(test_time, conc_CO, 'parent', CO_axes, 'color', 'k');
+    plot_CO2 = plot(test_time, conc_CO2, 'parent', CO2_axes, 'color', 'r');
     plot_HRR = plot(test_time, Q_OC__O2_CO2_CO, 'parent', HRR_axes, 'color', 'r');
     plot_duct_flow = plot(test_time, v_duct, 'parent', duct_flow_axes, 'color', 'g');
     toc
-    %
+    
+    ENG_data(i,:) = [timestamp(i) test_time(i) v_data(i,:) temperatures(i,:) test_time(i) mass(i) conc_O2(i) conc_CO(i) conc_CO2(i) DPT_Pa(i) RH_ambient(i) T_cold_trap(i) T_duct(i) T_smoke(i) T_duct2(i) T_ambient(i) Q_OC__O2_CO2_CO(i) HFG_VDC(i,:) TC_Temps(i,:)];
+    
+    
+    %        save some temporary data just in case
+    tic
+    %         writematrix(v_data, 'TEMP_vdata.txt')
+    %         writematrix(temperatures, 'TEMP_temp.txt')
+    %         writematrix(timestamp, 'TEMP_time.txt')
+    %         writematrix(TC_Temps, 'TEMP_TC_temps.txt')
+    writematrix(ENG_data, [FileName '.txt'])
+    toc
+    %     %
     i = i+1 %add one to the counting variable whch is used in indexing
     pause(0.1) %if there isnt a pause you cant click the buttons (hacky hack...)
     
 end
 
 %save the variables
-save(FileName)%, '-append')
+%save(FileName)%, '-append')
 
 % 'timestamp', 'v_data', 'temperatures', 'test_time', 'mass',...
 %     'O2_conc', 'CO_conc', 'CO2_conc', 'DPT_Pa', 'RH_ambient',...
@@ -147,10 +250,17 @@ save(FileName)%, '-append')
 %     'ignition_row', 'ignition_time', 'event_row', 'event_time',  'flameout_row', 'flameout_time',...
 %     'Q_OC__O2_CO2_CO', '-append')
 
+%save the variables
+save([FileName '.mat'])%, '-append')
 %Close and delete the objects
 fclose(v34970A);
 delete(v34970A);
-clear v34970A;
+if isobject(v34970A) == 1
+    clear v34970A;
+    fclose(v34980A);
+    delete(v34980A);
+    clear v34980A;
+end
 if isobject(loadcell) == 1
     fclose(loadcell);
     delete(loadcell);
@@ -199,7 +309,7 @@ RH_zero_span = [0 100];
 O2_fit = polyfit(O2_calib_V, O2_zero_span, 1);
 CO_fit = polyfit(CO_calib_V, CO_zero_span, 1);
 CO2_fit = polyfit(CO2_calib_V, CO2_zero_span, 1);
-DPT_fit = polyfit(DPT_calib_V, DPT_zero_span, 1);
+DPT_fit = polyfit([DPT_calib_V], DPT_zero_span, 1);
 RH_fit = polyfit(RH_calib_V, RH_zero_span, 1);
 
 save(FileName, 'O2_calib_V', 'CO_calib_V', 'CO2_calib_V', 'DPT_calib_V',...
@@ -207,7 +317,7 @@ save(FileName, 'O2_calib_V', 'CO_calib_V', 'CO2_calib_V', 'DPT_calib_V',...
 
 end
 
-function [Q_OC__O2_CO2_CO, v_duct, X0_O2_A, X0_CO_A, X0_CO2_A] = HRR_calc(i, CO_conc, CO2_conc, O2_conc, T_ambient, T_duct, RH_ambient, DPT_Pa, v_duct, X0_O2_A, X0_CO_A, X0_CO2_A,Q_OC__O2_CO2_CO)
+function [Q_OC__O2_CO2_CO, v_duct, X0_O2_A, X0_CO_A, X0_CO2_A] = HRR_calc(i, conc_CO, conc_CO2, conc_O2, T_ambient, T_duct, RH_ambient, DPT_Pa, v_duct, X0_O2_A, X0_CO_A, X0_CO2_A,Q_OC__O2_CO2_CO)
 %%% MOST OF THE INDEXING HERE CAN GO! %%%%
 %turn T into units of K
 T_ambient = T_ambient+273;
@@ -235,7 +345,7 @@ E_CO = 17.6;
 E_CO_CO2 = 17.63 * 1000; % no idea what this is! assumed to be the energy release from CO->CO2
 E_S = 12.3 * 1000;  % 12.3E3[MJ/kg]  Energy release per mass unit of oxygen consumed for combustion of soot to CO2 [kJ/kg]
 Delta_H_CO = 283000/28/1000 * 1000; % 2.83E5[kJ/mole] or 10.11 [MJ/kg] Energy release per unit mass of CO consumed in the burning of CO [kJ/mole] Ref.VI
-Delta_H_S = 393500/12/1000 * 1000; 
+Delta_H_S = 393500/12/1000 * 1000;
 
 % Calorimetry specific constants
 alpha = 1.105;
@@ -256,9 +366,9 @@ m_duct(i) = 26.54.*Area_Duct.*K./f_Re.*(DPT_Pa(i)./T_duct(i)).^(1/2);
 v_duct(i) = m_duct(i)./rho_air(i).*1000;
 
 % Mole fractions &c
-X_CO_A(i) = CO_conc(i) ./ 10^6;
-X_CO2_A(i) = CO2_conc(i) ./ 10^6;
-X_O2_A(i) = O2_conc(i) ./ 100;
+X_CO_A(i) = conc_CO(i) ./ 10^6;
+X_CO2_A(i) = conc_CO2(i) ./ 10^6;
+X_O2_A(i) = conc_O2(i) ./ 100;
 
 p_H2O_saturation(i) = exp(23.2 - 3816 ./ (-46 + T_ambient(i)));
 X0_H2O(i) = RH_ambient(i) .* p_H2O_saturation(i) ./ 100 ./ p_ambient;   % The molar fraction of water vapor Equation (10) Ref.I
@@ -274,10 +384,10 @@ X_CO2(i) = X_CO2_A(i) .* (1 - X_H2O(i) - X_Soot);
 % the ambinet mole fractions are taken to be the first reading to avoid
 % having to average
 if i==1
-X0_O2_A = X_O2_A(1);
-X0_CO2_A = X_CO2_A(1);
-X0_CO_A = X_CO_A(1);
-else 
+    X0_O2_A = X_O2_A(1);
+    X0_CO2_A = X_CO2_A(1);
+    X0_CO_A = X_CO_A(1);
+else
 end
 %commented sicne we dont calculate a pre ignition average
 % X0_O2_A = mean(X_O2_A(1:t_ignition_row-1));
